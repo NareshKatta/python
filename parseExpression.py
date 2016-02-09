7547f24d691eeecb30d6ddeaa13b14d1e8d4a47d
nareshkatta <katta.nareshbabu@gmail.com>
2016-02-09 15:31:47 +0530

    more changes

--- parseExpression.py
+++ parseExpression.py
@@ -1,5 +1,6 @@
 from Stack import Stack
 from BinaryTree import BinaryTree
+import operator
 
 def parseExpression(expression):
 	tokens = expression.split()
@@ -27,6 +28,16 @@ def parseExpression(expression):
 			raise ValueError
 	return tree
 
+def evaluate(parseTree):
+    operators = {'+' : operator.add, '-' : operator.sub, '*' : operator.mul, '/' : operator.truediv}
+    leftc = parseTree.getLeftChild()
+    rightc = parseTree.getRightChild()
+    if leftc and rightc:
+        fn = operators[parseTree.getRootVal()]
+        return fn(evaluate(leftc), evaluate(rightc))
+    else:
+        return parseTree.getRootVal()
+
 t = parseExpression(' ( 3 + ( 4 * 5 ) )')
 t.postorder()
-
+print "Value of the expression is", evaluate(t);
